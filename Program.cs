d95dd9c84e94eadb2543afb03d719f2f7e009f01
Eduardo-072 <eduardosilvaaa072@gmail.com>
2023-08-19 19:44:48 -0300

    foi ultilizado os comandos thread.sleep; console.beep; e a cola do professor no github

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿// Dó, ré, mi, fá, fá, fá
+Console.Beep(1320, 200);
+Console.Beep(1485, 200);
+Console.Beep(1650, 200);
+Console.Beep(1759, 200);
+
+Thread.Sleep(500);
+
+Console.Beep(1759, 200);
+Console.Beep(1759, 200);
+
+Thread.Sleep(1000);
+
+// Dó, ré, dó, ré, ré, ré
+Console.Beep(1320, 200);
+Console.Beep(1485, 200);
+Console.Beep(1320, 200);
+Console.Beep(1485, 200);
+
+Thread.Sleep(500);
+
+Console.Beep(1485, 200);
+Console.Beep(1485, 200);
+
+Thread.Sleep(1000);
+
+// Dó, sol, fá, mi, mi ,mi
+Console.Beep(1320, 200);
+Console.Beep(1980, 200);
+Console.Beep(1759, 200);
+Console.Beep(1650, 200);
+
+Thread.Sleep(500);
+
+Console.Beep(1650, 200);
+Console.Beep(1650, 200);
+
+Thread.Sleep(1000);
+
+// Dó, ré, mi, fá, fá, fá
+Console.Beep(1320, 200);
+Console.Beep(1485, 200);
+Console.Beep(1650, 200);
+Console.Beep(1759, 200);
+
+Thread.Sleep(500);
+
+Console.Beep(1759, 200);
+Console.Beep(1759, 200);
+
+Thread.Sleep(1000);
+
+
+
+
+
+
+
